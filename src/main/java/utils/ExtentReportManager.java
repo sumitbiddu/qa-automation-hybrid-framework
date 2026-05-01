5a3e1218ccb160ddce66f2d477d65177ba573d46
sumit.sisodia27@gmail.comgit config --global user.namegit config --global user.name sumit.sisodia27@gmail.com <sumit.sisodia27@gmail.com>
2026-04-30 22:58:42 -0300

    Fix extent report initialization and CI stability

--- src/main/java/utils/ExtentReportManager.java
+++ src/main/java/utils/ExtentReportManager.java
@@ -3,20 +3,34 @@ package utils;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
+import java.io.File;
+
 public class ExtentReportManager {
+
+    private static ExtentReports extent;
 
     public static ExtentReports getReportObject() {
 
-        String path = System.getProperty("user.dir") + "/reports/index.html";
+        if (extent == null) {
 
-        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
-        reporter.config().setReportName("QA Automation Results");
-        reporter.config().setDocumentTitle("Test Report");
+            // 🔥 FORCE ROOT SAFE PATH
+            String path = System.getProperty("user.dir") + "/reports/index.html";
 
-        ExtentReports extent = new ExtentReports();
-        extent.attachReporter(reporter);
+            File reportDir = new File(System.getProperty("user.dir") + "/reports");
+            if (!reportDir.exists()) {
+                reportDir.mkdirs();
+            }
 
-        extent.setSystemInfo("Tester", "Sumit");
+            ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+            reporter.config().setReportName("QA Automation Results");
+            reporter.config().setDocumentTitle("Test Report");
+
+            extent = new ExtentReports();
+            extent.attachReporter(reporter);
+
+            extent.setSystemInfo("Tester", "Sumit");
+            extent.setSystemInfo("Env", "CI + Local");
+        }
 
         return extent;
     }
--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -1,70 +1,66 @@
 package utils;
 
 import base.BaseUI;
-
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 import java.io.File;
-import java.io.IOException;
 
 public class TestListener implements ITestListener {
 
-    //  Screenshot method
-    public void captureScreenshot(WebDriver driver, String testName) {
-        File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        File dest = new File("screenshots/" + testName + ".png");
+	ExtentReports extent;
+    ThreadLocal<ExtentTest> test = new ThreadLocal<>();
 
-        try {
-            FileUtils.copyFile(src, dest);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public void onStart(ITestContext context) {
+        extent = ExtentReportManager.getReportObject();
     }
 
-    //  On Test Failure
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.get().pass("Test Passed");
+    }
+
     @Override
     public void onTestFailure(ITestResult result) {
+        test.get().fail(result.getThrowable());
+
         try {
             WebDriver driver = BaseUI.driver;
 
-            if (driver == null) {
-                System.out.println("Driver is null, cannot take screenshot");
-                return;
-            }
+            if (driver == null) return;
 
             File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
-            File folder = new File(System.getProperty("user.dir") + "/screenshots");
+            String folderPath = System.getProperty("user.dir") + "/screenshots/";
+            File folder = new File(folderPath);
             if (!folder.exists()) {
                 folder.mkdirs();
             }
 
-            String fileName = result.getName() + "_" + System.currentTimeMillis() + ".png";
+            String fileName = result.getMethod().getMethodName()
+                    + "_" + System.currentTimeMillis() + ".png";
 
-            File dest = new File(folder, fileName);
-
+            File dest = new File(folderPath + fileName);
             FileUtils.copyFile(src, dest);
 
-            System.out.println("Screenshot saved at: " + dest.getAbsolutePath());
+            test.get().addScreenCaptureFromPath("screenshots/" + fileName);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    // (Optional but good for logs)
     @Override
-    public void onTestSuccess(ITestResult result) {
-        System.out.println("Test Passed: " + result.getName());
-    }
+    public void onFinish(ITestContext context) {
+    	String path = "reports/index.html";
+        System.out.println("🔥 EXTENT REPORT GENERATED AT: " + path);
 
-    @Override
-    public void onTestStart(ITestResult result) {
-        System.out.println("Test Started: " + result.getName());
+        extent.flush();
     }
 }
